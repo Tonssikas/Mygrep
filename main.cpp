482391c96638ae097e80609f57aa8bcfc7bfa239
Tonssikas <150654164+Tonssikas@users.noreply.github.com>
2024-02-12 13:43:16 +0200

    3. Inkrementti
    Työ vastaa nyt 3. inkrementtiä

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 
 //prototypes
-void searchString(std::string search_string, std::string input_string);
-void searchFile(std::string fileName, std::string search_string);
+void searchString(const std::string& search_string, const std::string& input_string);
+void searchFile(const std::string& fileName, const std::string& search_string, bool showLineNumbers = false, bool countOccurrences = false);
 
 
 
@@ -26,11 +26,28 @@ int main(int argc, char *argv[]) {
 
         } else if (argc == 3) {
 
-            std::string fileName = argv[1];
-            std::string search_string = argv[2];
+            std::string fileName = argv[2];
+            std::string search_string = argv[1];
 
             searchFile(fileName, search_string);
+        } else if (argc == 4) {
+            std::string fileName = argv[3];
+            std::string search_string = argv[2];
+            std::string option = argv[1];
+
+            if (option == "-ol") {
+                searchFile(fileName, search_string, true);
+            } else if (option == "-oo") {
+                searchFile(fileName, search_string, false, true);
+            } else if (option == "-olo" || "-ool") {
+                searchFile(fileName, search_string, true, true);
+            } else {
+                std::cout << "Invalid option" << std::endl;
+            }
+        } else {
+            std::cout << "Invalid number of arguments" << std::endl;
         }
+
 
 
 
@@ -38,30 +55,37 @@ int main(int argc, char *argv[]) {
         return 0;
 }
 
-void searchString(std::string search_string, std::string input_string) {
+void searchString(const std::string& search_string, const std::string& input_string) {
     int found = input_string.find(search_string);
     if (found != std::string::npos) {
-        std::cout << "'" << search_string << "' Found in '" << input_string << "' in position " << found
-                  << std::endl;
+        std::cout << "'" << search_string << "' Found in '" << input_string << "' in position " << found << std::endl;
     } else {
         std::cout << "'" << search_string << "' NOT found in '" << input_string << "'" << std::endl;
     }
 }
 
-void searchFile(std::string fileName, std::string search_string) {
+void searchFile(const std::string& fileName, const std::string& search_string, bool showLineNumbers, bool countOccurrences) {
     std::ifstream file(fileName);
     if (!file.is_open()) {
         std::cout << "File '" << fileName << "' not found" << std::endl;
         return;
     }
     std::string line;
+     int lineCount = 1;
+     int occurrences = 0;
     while (getline(file, line)) {
 
         int found = line.find(search_string);
         if (found != std::string::npos) {
-            std::cout << line << std::endl;
+            occurrences++;
+            if (showLineNumbers) {
+                std::cout << lineCount << ": " << line << std::endl;
+            } else std::cout << line << std::endl;
         }
-
+        lineCount++;
+    }
+    if (countOccurrences) {
+        std::cout << "\n" << "Occurrences of lines containing '" << search_string << "': " << occurrences << std::endl;
     }
     file.close();
 }
